61d25683d7f86a9b4f36a1c1d3f7fe1864bb1878
Alireza Tehrani <44713526+AlirezaGhafartehrani@users.noreply.github.com>
2019-07-01 10:26:35 +0430

    Add files via upload

---
+++ Lab.10/src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    private String username;
+    private String userMessage;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+}
